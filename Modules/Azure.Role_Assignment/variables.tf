3038d91ec2826632bc712c418a5bd02b9c339b8e
abhilashasr21 <abhilashasr@microsoft.com>
2026-03-30 19:44:44 +0530

    Adding Code for role assignment

---
+++ Modules/Azure.Role_Assignment/variables.tf
@@ -0,0 +1,4 @@
+variable "role_definition_name" {
+  description = "The name of the role definition to assign."
+  type        = string  
+}
--- main.tf
+++ main.tf
@@ -34,3 +34,7 @@ module "subscription_association" {
   subscription_id     = "/subscriptions/f4a270f4-c469-4215-bef6-b4abaea6815e"
   management_group_id = module.management_group_child.management_group_id
 }
+
+#****************************************************************#
+#                        Role Assignment                         #
+#****************************************************************#

e881b127ff6e470151f99acdd4648a4d6922857d
abhilashasr21 <abhilashasr@microsoft.com>
2026-05-07 15:05:01 +0530

    corrected the code flow

--- tenant/main.tf
+++ tenant/main.tf
@@ -1,3 +1,4 @@
+# 
 #create Variable for management groups
 variable "management_groups" {
   description = "A map of management groups to create. The key is the name of the management group, and the value is an object containing the display name and parent ID."
@@ -9,8 +10,8 @@ variable "management_groups" {
 
 #create block for management group tf resource
 resource "azurerm_management_group" "example" {
-for_each    = var.management_groups
-  name        = each.key
+  for_each     = var.management_groups
+  name         = each.key
   display_name = each.value.display_name
-  parent_id    = each.value.parent_id != null ? each.value.parent_id : null
+  # parent_id    = each.value.parent_id != null ? each.value.parent_id : null
 }

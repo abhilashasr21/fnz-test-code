a62eca732055a891fb0c5656f4fd89f2a3509f9d
abhilashasr21 <abhilashasr@microsoft.com>
2026-07-13 13:02:52 +0530

    creating pipeline to check oauth authentication

--- azapi_mgmt_grp/main.tf
+++ azapi_mgmt_grp/main.tf
@@ -1,7 +1,7 @@
 resource "azapi_resource" "management_group_parent" {
   type      = "Microsoft.Management/managementGroups@2021-04-01"
   name      = "mg201"
-  parent_id = "/"  
+  parent_id = "/"
 
   body = {
     properties = {
@@ -10,7 +10,36 @@ resource "azapi_resource" "management_group_parent" {
   }
 }
 
-resource "azurerm_management_group" "management_group_child" {
-  display_name = "Terraform Child Management Group"
-  parent_management_group_id = azapi_resource.management_group_parent.id
+resource "azapi_resource" "management_group_child" {
+  type      = "Microsoft.Management/managementGroups@2021-04-01"
+  name      = "mg202"
+  parent_id = "/"
+
+  body = {
+    properties = {
+      displayName = "Terraform Child Management Group"
+      details = {
+        parent = {
+          id = azapi_resource.management_group_parent.id
+        }
+      }
+    }
+  }
+}
+
+resource "azapi_resource" "management_group_child2" {
+  type      = "Microsoft.Management/managementGroups@2021-04-01"
+  name      = "mg112"
+  parent_id = "/"
+
+  body = {
+    properties = {
+      displayName = "Terraform Child 2 Management Group"
+      details = {
+        parent = {
+          id = azapi_resource.management_group_parent.id
+        }
+      }
+    }
+  }
 }

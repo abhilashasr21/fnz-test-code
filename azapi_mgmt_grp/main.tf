cca701835d6ac15c4dce0a53274094fca06e690a
abhilashasr21 <abhilashasr@microsoft.com>
2026-07-13 14:54:07 +0530

    Importing second Management Group

--- azapi_mgmt_grp/main.tf
+++ azapi_mgmt_grp/main.tf
@@ -10,22 +10,22 @@ resource "azapi_resource" "management_group_parent" {
   }
 }
 
-# resource "azapi_resource" "management_group_child" {
-#   type      = "Microsoft.Management/managementGroups@2021-04-01"
-#   name      = "mg202"
-#   parent_id = "/"
+resource "azapi_resource" "management_group_child" {
+  type      = "Microsoft.Management/managementGroups@2021-04-01"
+  name      = "mg202"
+  parent_id = "/"
 
-#   body = {
-#     properties = {
-#       displayName = "Terraform Child Management Group"
-#       details = {
-#         parent = {
-#           id = azapi_resource.management_group_parent.id
-#         }
-#       }
-#     }
-#   }
-# }
+  body = {
+    properties = {
+      displayName = "Terraform Child Management Group"
+      details = {
+        parent = {
+          id = azapi_resource.management_group_parent.id
+        }
+      }
+    }
+  }
+}
 
 # resource "azapi_resource" "management_group_child2" {
 #   type      = "Microsoft.Management/managementGroups@2021-04-01"

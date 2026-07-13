7722527223df7976aafbe5e4d7086a6270321b5a
abhilashasr21 <abhilashasr@microsoft.com>
2026-07-13 14:18:44 +0530

    testing Import pipeline

--- azapi_mgmt_grp/backend.tf
+++ azapi_mgmt_grp/backend.tf
@@ -4,6 +4,12 @@ terraform {
       source  = "azure/azapi"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "tfsttaate"
+    storage_account_name = "tfstatemgmtimporttest"
+    container_name       = "container"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azapi" {

11d0c4546e0584129a11f609e13b19abf8f721b6
abhilashasr21 <abhilashasr@microsoft.com>
2026-07-13 14:52:55 +0530

    Commenting other management group

--- azapi_mgmt_grp/main.tf
+++ azapi_mgmt_grp/main.tf
@@ -10,36 +10,36 @@ resource "azapi_resource" "management_group_parent" {
   }
 }
 
-resource "azapi_resource" "management_group_child" {
-  type      = "Microsoft.Management/managementGroups@2021-04-01"
-  name      = "mg202"
-  parent_id = "/"
+# resource "azapi_resource" "management_group_child" {
+#   type      = "Microsoft.Management/managementGroups@2021-04-01"
+#   name      = "mg202"
+#   parent_id = "/"
 
-  body = {
-    properties = {
-      displayName = "Terraform Child Management Group"
-      details = {
-        parent = {
-          id = azapi_resource.management_group_parent.id
-        }
-      }
-    }
-  }
-}
+#   body = {
+#     properties = {
+#       displayName = "Terraform Child Management Group"
+#       details = {
+#         parent = {
+#           id = azapi_resource.management_group_parent.id
+#         }
+#       }
+#     }
+#   }
+# }
 
-resource "azapi_resource" "management_group_child2" {
-  type      = "Microsoft.Management/managementGroups@2021-04-01"
-  name      = "mg112"
-  parent_id = "/"
+# resource "azapi_resource" "management_group_child2" {
+#   type      = "Microsoft.Management/managementGroups@2021-04-01"
+#   name      = "mg112"
+#   parent_id = "/"
 
-  body = {
-    properties = {
-      displayName = "Terraform Child 2 Management Group"
-      details = {
-        parent = {
-          id = azapi_resource.management_group_parent.id
-        }
-      }
-    }
-  }
-}
+#   body = {
+#     properties = {
+#       displayName = "Terraform Child 2 Management Group"
+#       details = {
+#         parent = {
+#           id = azapi_resource.management_group_parent.id
+#         }
+#       }
+#     }
+#   }
+# }

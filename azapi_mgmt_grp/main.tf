14881c82de88f5cf00135e27665f9c337c53cefe
abhilashasr21 <abhilashasr@microsoft.com>
2026-07-14 13:46:35 +0530

    commenting extra code

--- azapi_mgmt_grp/main.tf
+++ azapi_mgmt_grp/main.tf
@@ -1,32 +1,31 @@
-resource "azapi_resource" "management_group_parent" {
-  type      = "Microsoft.Management/managementGroups@2021-04-01"
-  name      = "mg201"
-  parent_id = "/"
+# resource "azapi_resource" "management_group_parent" {
+#   type      = "Microsoft.Management/managementGroups@2021-04-01"
+#   name      = "mg201"
+#   parent_id = "/"
 
-  body = {
-    properties = {
-      displayName = "Terraform Parent Management Group"
-    }
-  }
-}
+#   body = {
+#     properties = {
+#       displayName = "Terraform Parent Management Group"
+#     }
+#   }
+# }
 
-resource "azapi_resource" "management_group_child" {
-  type      = "Microsoft.Management/managementGroups@2021-04-01"
-  name      = "mg202"
-  parent_id = "/"
+# resource "azapi_resource" "management_group_child" {
+#   type      = "Microsoft.Management/managementGroups@2021-04-01"
+#   name      = "mg202"
+#   parent_id = "/"
 
-  body = {
-    properties = {
-      displayName = "Terraform Child Management Group"
-      details = {
-        parent = {
-          id = azapi_resource.management_group_parent.id
-        }
-      }
-    }
-  }
-}
-
+#   body = {
+#     properties = {
+#       displayName = "Terraform Child Management Group"
+#       details = {
+#         parent = {
+#           id = azapi_resource.management_group_parent.id
+#         }
+#       }
+#     }
+#   }
+# }
 resource "azapi_resource" "management_group_child2" {
   for_each = var.child_management_groups
 

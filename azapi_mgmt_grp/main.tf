79eef3051cbd2c836d356e18f848d8ad33c674bd
abhilashasr21 <abhilashasr@microsoft.com>
2026-07-14 14:00:07 +0530

    fixing error

--- azapi_mgmt_grp/main.tf
+++ azapi_mgmt_grp/main.tf
@@ -38,7 +38,7 @@ resource "azapi_resource" "management_group_child2" {
       displayName = each.value.display_name
       details = {
         parent = {
-          id = each.value.parent_id != null ? ("/providers/Microsoft.Management/managementGroups/${each.value.parent_id}") : azapi_resource.management_group_parent.id
+          id = each.value.parent_id != null ? ("/providers/Microsoft.Management/managementGroups/${each.value.parent_id}") : azapi_resource.management_group_parent.parent_id
         }
       }
     }
